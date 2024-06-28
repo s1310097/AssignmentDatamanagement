7ea48db5f5b6e00b897d2cc723560ceed7e1aa16
pika <s1310097@u-aizu.ac.jp>
2024-06-28 12:27:49 +0900

    HoT: initial revision

---
+++ HeadOrTail.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    random_device seed_generator;
+    mt19937 engine(seed_generator());
+    bernoulli_distribution distribution(0.5);
+    int headsCounter = 0;
+    int tailsCounter = 0;
+
+
+    printf("Tossing a coin...\n");
+
+    for(int i = 0; i < 3 ; i++){
+        printf("Round %d: ",i + 1);
+
+        switch(distribution(engine))
+        {
+            case true:
+                printf("Heads");
+                headsCounter++;
+                break;
+            case false:
+                printf("Tails");
+                tailsCounter++;
+                break;
+        }
+
+        printf("\n");
+
+    }
+
+    printf("Heads: %d, Tails: %d\n",headsCounter,tailsCounter);
+}
+
