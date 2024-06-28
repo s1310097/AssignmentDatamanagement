8ca376327c515347ab3be5ad4b4a555fb96df72a
pika <s1310097@u-aizu.ac.jp>
2024-06-28 12:41:54 +0900

    User name added

--- HeadOrTail.cpp
+++ HeadOrTail.cpp
@@ -1,38 +1,16 @@
 #include <stdio.h>
 #include <random>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    random_device seed_generator;
-    mt19937 engine(seed_generator());
-    bernoulli_distribution distribution(0.5);
-    int headsCounter = 0;
-    int tailsCounter = 0;
+    char str[100];
 
+    printf("Who are you?\n");
+    scanf("%99s", str);  // %99s を使ってバッファサイズを超えないようにする
+    printf("Hello, %s\n", str);
 
-    printf("Tossing a coin...\n");
-
-    for(int i = 0; i < 3 ; i++){
-        printf("Round %d: ",i + 1);
-
-        switch(distribution(engine))
-        {
-            case true:
-                printf("Heads");
-                headsCounter++;
-                break;
-            case false:
-                printf("Tails");
-                tailsCounter++;
-                break;
-        }
-
-        printf("\n");
-
-    }
-
-    printf("Heads: %d, Tails: %d\n",headsCounter,tailsCounter);
 }
 
